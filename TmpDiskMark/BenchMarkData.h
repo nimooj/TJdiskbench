4829ddf49efb120e3681de5591f6ef2af26eda57
mjkang <mj.kang@thewhalegames.com>
2016-12-02 15:29:35 +0900

    benchmarkdata update

--- TmpDiskMark/BenchMarkData.h
+++ TmpDiskMark/BenchMarkData.h
@@ -8,25 +8,45 @@ public:
 	int trials = 1;
 	double pageSize;
 
+  double seqRead_4K;
+  double seqRead_16K;
 	double seqRead_64K;
 	double seqRead_128K;
 	double seqRead_256K;
 	double seqRead_512K;
+  double seqRead_1G;
 	double seqRead_2G;
 	double seqRead_4G;
-	double seqRead_8G;
-	double seqRead_16G;
-	double seqRead_32G;
 
+  double randRead_4K;
+  double randRead_16K;
 	double randRead_64K;
 	double randRead_128K;
 	double randRead_256K;
 	double randRead_512K;
+  double randRead_1G;
 	double randRead_2G;
 	double randRead_4G;
-	double randRead_8G;
-	double randRead_16G;
-	double randRead_32G;
+
+  double seqWrite_4K;
+  double seqWrite_16K;
+	double seqWrite_64K;
+	double seqWrite_128K;
+	double seqWrite_256K;
+	double seqWrite_512K;
+  double seqWrite_1G;
+	double seqWrite_2G;
+	double seqWrite_4G;
+
+  double randWrite_4K;
+  double randWrite_16K;
+	double randWrite_64K;
+	double randWrite_128K;
+	double randWrite_256K;
+	double randWrite_512K;
+  double randWrite_1G;
+	double randWrite_2G;
+	double randWrite_4G;
 
 	double migration_128G;
 	double migration_256G;
